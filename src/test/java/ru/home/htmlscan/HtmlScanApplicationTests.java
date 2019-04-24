ac55355a731672dc3f5d01a9e2b590f2b3bb7b79
Козлов Антон <kozlovav@nis-glonass.ru>
2019-04-24 13:45:16 +0300

    Доработал тесты

--- src/test/java/ru/home/htmlscan/HtmlScanApplicationTests.java
+++ src/test/java/ru/home/htmlscan/HtmlScanApplicationTests.java
@@ -1,37 +1,58 @@
 package ru.home.htmlscan;
 
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.home.htmlscan.config.HtmlScanProperties;
 import ru.home.htmlscan.service.MailService;
 
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Profile("test")
+@Slf4j
 public class HtmlScanApplicationTests {
 
 	@Autowired
 	private ResourceLoader resourceLoader;
 	@Autowired
-	private MailService mailService;
-	@Autowired
 	private HtmlScanProperties properties;
+	@Mock
+	private JavaMailSender testSender;
+
+	private MailService mailService;
+
+	@Before
+	public void setUp() {
+		val mimeMessage = new MimeMessage((Session) null);
+		doNothing().when(testSender).send(any(MimeMessage.class));
+		when(testSender.createMimeMessage()).thenReturn(mimeMessage);
+		mailService = new MailService(testSender);
+	}
 
 	@Test
-	public void contextLoads() {
+	public void checkOpenedTest() {
 		val siteOpened = resourceLoader.getResource("classpath:site_opened.html");
 		assertNotNull(siteOpened);
 		val siteClosed = resourceLoader.getResource("classpath:site_closed.html");
@@ -45,12 +66,14 @@ public class HtmlScanApplicationTests {
 				mailService.checkSite(u, htmlClosed);
 			});
 		} catch (IOException e) {
+			log.error(e.getMessage());
 			e.printStackTrace();
 		}
 
 		try {
 			TimeUnit.SECONDS.sleep(10L);
 		} catch (InterruptedException e) {
+			log.error(e.getMessage());
 			e.printStackTrace();
 		}
 	}
