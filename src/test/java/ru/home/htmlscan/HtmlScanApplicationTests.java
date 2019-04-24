920a42b8b4340c4ba497680516ac2d92984c6685
Козлов Антон <kozlovav@nis-glonass.ru>
2019-04-24 08:58:02 +0300

    Изменил адрес почты

--- src/test/java/ru/home/htmlscan/HtmlScanApplicationTests.java
+++ src/test/java/ru/home/htmlscan/HtmlScanApplicationTests.java
@@ -1,26 +1,16 @@
 package ru.home.htmlscan;
 
-import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Profile("test")
-@Slf4j
 public class HtmlScanApplicationTests {
-
-	@Autowired
-	private ResourceLoader resourceLoader;
 
 	@Test
 	public void contextLoads() {
-		resourceLoader.getResource("classpath:site_opened.html");
 	}
+
 }
