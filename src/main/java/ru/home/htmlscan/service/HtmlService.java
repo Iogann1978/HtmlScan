992557e42e590e4030d52048e93a366ca8a36c45
Козлов Антон <kozlovav@nis-glonass.ru>
2019-04-25 15:17:14 +0300

    Доработал тест для Rest-контроллера

--- src/main/java/ru/home/htmlscan/service/HtmlService.java
+++ src/main/java/ru/home/htmlscan/service/HtmlService.java
@@ -3,7 +3,6 @@ package ru.home.htmlscan.service;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Async;
