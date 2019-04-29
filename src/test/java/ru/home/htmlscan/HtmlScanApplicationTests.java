82a5edcc3ebf00dbe1e1229fa06604bcc0d9182e
Козлов Антон <kozlovav@nis-glonass.ru>
2019-04-29 13:36:45 +0300

    Исправил тесты

--- src/test/java/ru/home/htmlscan/HtmlScanApplicationTests.java
+++ src/test/java/ru/home/htmlscan/HtmlScanApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -28,7 +27,6 @@ import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 
